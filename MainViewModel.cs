786a6607de92160b49ef407033b447477886bdd3
HarriYin <15655379827@163.com>
2022-06-17 14:18:23 +0800

    添加项目文件。

---
+++ MainViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingSample
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+
+            ShowCommand = new MyCommand(Show);
+        }
+
+        public MyCommand ShowCommand { get; set; }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                //name = value;
+                //OnPropertyChanged();
+
+                UpdateProper(ref name, value);
+            }
+        }
+
+
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                //title = value;
+                //OnPropertyChanged();
+
+                UpdateProper(ref title, value);
+            }
+        }
+
+
+
+        private void Show()
+        {
+            Name = "点击了按钮";
+            Title = "我是标题";
+            System.Windows.MessageBox.Show(Name);
+        }
+
+
+    }
+}
---
+++ ViewModelBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingSample
+{
+    //关于 CallerMemberName ，https://blog.csdn.net/qq_43024228/article/details/119147858 -> 学习
+
+    public class ViewModelBase : System.ComponentModel.INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// 对事件进行封装，便于调用
+        /// 对输入参数 propertyName 添加特性 -> CallerMemberName ，用于获取调用方的方法或属性名称
+        /// </summary>
+        /// <param name="propertyName"></param>
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void UpdateProper<T>(ref T properValue, T newValue, [CallerMemberName] string properName = "")
+        {
+            if (object.Equals(properValue, newValue))
+                return;
+
+            properValue = newValue;
+            OnPropertyChanged(properName);
+        }
+    }
+}
